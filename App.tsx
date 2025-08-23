bcf4230c7ef178dd6eef9347dda52b635c38e38b
jvcbyte <jvc8463@gmail.com>
2025-08-23 13:51:11 +0100

    Feat: add basic sign in screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Sign in</Text>
+      <TextInput placeholder="Email" style={styles.input} />
+      <TextInput placeholder="Password" style={styles.input} secureTextEntry />
+      
+      <Pressable style={styles.button} onPress={() => { console.log('Sign in button pressed') }}>
+        <Text style={styles.buttonText}>Sign in</Text>
+      </Pressable>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +21,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    gap: 20,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    borderRadius: 5,
+  },
+
+  button: {
+    backgroundColor: '#4353fd',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+    // textAlign: 'center',
   },
 });
