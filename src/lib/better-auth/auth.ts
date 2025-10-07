acfbe4e267ecf8f72ce0e21aae2b70adb9cfe84c
jvcbyte <jvc8463@gmail.com>
2025-10-07 18:05:04 +0100

    add pg

--- src/lib/better-auth/auth.ts
+++ src/lib/better-auth/auth.ts
@@ -1,5 +1,8 @@
 import { betterAuth } from "better-auth";
+import { Pool } from "pg";
 
 export const auth = betterAuth({
-    //...
-});
+    database: new Pool({
+        // connection options
+    }),
+})
